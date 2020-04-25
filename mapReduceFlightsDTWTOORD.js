3ac96788de82874a4abb79121613c79732fb44aa
ishaank95 <63068646+ishaank95@users.noreply.github.com>
2020-04-25 09:24:50 -0400

    Map reduce javascript files

---
+++ mapReduceFlightsDTWTOORD.js
@@ -0,0 +1,57 @@
+// *** 3T Software Labs, Studio 3T: MapReduce Job ****
+
+// Variable for db
+var __3tsoftwarelabs_db = "test";
+
+// Variable for map
+var __3tsoftwarelabs_map = function () {
+
+    // Enter the JavaScript for the map function here
+    // You can access the current document as 'this'
+    //
+    // Available functions: assert(), BinData(), DBPointer(), DBRef(), doassert(), emit(), gc()
+    //                      HexData(), hex_md5(), isNumber(), isObject(), ISODate(), isString()
+    //                      Map(), MD5(), NumberInt(), NumberLong(), ObjectId(), print()
+    //                      printjson(), printjsononeline(), sleep(), Timestamp(), tojson()
+    //                      tojsononeline(), tojsonObject(), UUID(), version()
+    //
+    // Available properties: args, MaxKey, MinKey
+    
+    var key = this.AIRLINE
+    var value = NumberInt(this.ARRIVAL_DELAY)
+    
+
+    emit(key, value);
+}
+;
+
+// Variable for reduce
+var __3tsoftwarelabs_reduce = function (key, values) {
+
+    // Enter the JavaScript for the reduce function here
+    // 'values' is a list of objects as emit()'ed by the map() function
+    // Make sure the object your return is of the same type as the ones emit()'ed
+    //
+    // Available functions: assert(), BinData(), DBPointer(), DBRef(), doassert(), emit(), gc()
+    //                      HexData(), hex_md5(), isNumber(), isObject(), ISODate(), isString()
+    //                      Map(), MD5(), NumberInt(), NumberLong(), ObjectId(), print()
+    //                      printjson(), printjsononeline(), sleep(), Timestamp(), tojson()
+    //                      tojsononeline(), tojsonObject(), UUID(), version()
+    //
+    // Available properties: args, MaxKey, MinKey
+
+    var reducedValue = Array.sum(values);
+
+    return reducedValue;
+}
+;
+
+db.runCommand({ 
+    mapReduce: "Flights",
+    map: __3tsoftwarelabs_map,
+    reduce: __3tsoftwarelabs_reduce,
+    out: {"inline": 1},
+    query: {ORIGIN_AIRPORT: "DTW", DESTINATION_AIRPORT:"ORD"},
+    sort: {value: -1},
+    inputDB: "test",
+ });
